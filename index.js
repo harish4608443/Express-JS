34edaf4783565e993c2dfd2963434aa409ff7a76
harish4608443 <84072184+harish4608443@users.noreply.github.com>
2021-09-29 21:34:36 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,12 @@
+var path = require('path');
+var express = require('express');
+var app = express();
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, 'public')));
+app.post('/handler', function (req, res) {
+console.log(req.body);
+res.write(req.body.username);
+res.write(req.body.password);
+res.send(); // or res.end
+});
+app.listen(3000);
